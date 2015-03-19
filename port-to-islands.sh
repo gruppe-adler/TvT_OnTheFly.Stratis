cd381fe18ea6f9e4fb5c3b7043d7b1e0eee18eda
Moritz Schmidt <fusselwurm@gmail.com>
2015-03-19 23:49:37 +0100

    build pbo files for all maps
    	* be sure you have git bash installed
    	* put cpbo.exe in this directory
    	* double click on port-to-islands.sh
    	* watch inane output until its finished
    	* find pbo files in ../tmp-build
    	* pbos will have _$version in their file name.
    	  $name equals the current tag (i.e. release), if there is one
    	  if there's no current release, $version will be the first 7 chars of the latest commit hash

---
+++ port-to-islands.sh
@@ -0,0 +1,90 @@
+#!/bin/bash
+
+# change this to your local cpbo path, in case cpbo.exe isa not in the current dir
+cpbo_path="$1"
+
+islands=(
+	Altis
+	Bornholm
+	Chernarus
+	Chernarus_Summer
+	FDF_Isle1_a
+	Gorgona
+	IsolaDiCapraia
+	Panthera3
+	Sara_dbe1
+	Shapur_BAF
+	Stratis
+	Thirsk
+	Woodland_ACR
+	Zargabad
+	clafghan
+	fata
+	saru
+	takistan
+	xcam_prototype
+)
+
+### AS AS USER, DONT EDIT BELOW THIS LINE ###
+
+if [[ ! -f $cpbo_path ]]; then
+	cpbo_path="./cpbo.exe"
+fi
+
+if [[ ! -f $cpbo_path ]]; then
+	echo "warning: cpbo path not found, will not build pbo files!"
+	exit 1
+fi
+
+
+head=`git reflog --decorate -1 --no-color`
+
+# if possible, use current tag...
+#version=`echo $head | grep -o -E 'tag: \w+' | sed -e 's/tag: //'`
+version=`echo $head | sed -re 's/^.*tag: ([0-9a-z\.\-]+).*$/\1/'`
+
+if [[ "$head" == "$version" ]]; then
+	# ...if not, use commit hash
+	#	version=`echo $head | grep --color=never -o -E '^[0-9a-f]+'`
+	version=`echo $head | sed -re 's/^([0-9a-f]+).*$/\1/g'`
+fi
+
+echo $version
+
+if [[ $version = "" ]]; then
+	echo "cant find tag OR commit hash. are you sure we're having a .git directory here?"
+	exit 2
+fi
+
+missionname="TvT_OnTheFly_$version"
+
+echo "$missionname"
+
+
+cwd=`pwd`
+
+builddir="$cwd/../tmp-build"
+tmpdir="$builddir/tmpdir"
+
+mkdir -p $builddir
+
+cp -r ./ $tmpdir
+rm $tmpdir/*.sh
+rm $tmpdir/*.bat
+rm -fr $tmpdir/.git
+
+for island in "${islands[@]}"; do
+	islanddir="$builddir/$missionname.$island"
+	mv $tmpdir $islanddir
+	echo "building for $island...";
+	$cpbo_path -y -p $islanddir > /dev/null
+	echo "done (probably)"
+	mv $islanddir $tmpdir
+
+
+done
+
+echo "done packing. clean up..."
+rm -r $tmpdir
+echo "plz see $builddir for pbo files"
+sleep 2
