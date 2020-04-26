a7805c3c2236fd3c3f19cdfdebeb76e4e724faac
McDiod <hobbeldobbel@gmail.com>
2020-04-26 20:51:19 +0200

    close #107 add entrenching tool to heavy package

--- buyables/defenses/rus_msv.hpp
+++ buyables/defenses/rus_msv.hpp
@@ -2,6 +2,7 @@ class Defenses: defenses_base {
 
     class Land_HBarrier_5_F: Land_HBarrier_5_F {};
     class Land_BagFence_Long_F: Land_BagFence_Long_F {};
+    class ACE_EntrenchingTool: ACE_EntrenchingTool {};
     class B_Mortar_01_F: B_Mortar_01_F {};
 
     class rhs_KORD_high_MSV {
--- buyables/defenses_base.hpp
+++ buyables/defenses_base.hpp
@@ -22,11 +22,12 @@ class defenses_base {
         code = "if (player == _this select 0) then {[player,'Land_HBarrier_1_F',3] call grad_fortifications_fnc_addFort; [player,'Land_HBarrier_3_F',2] call grad_fortifications_fnc_addFort; [player,'Land_HBarrier_5_F',1] call grad_fortifications_fnc_addFort;};";
     };
 
-    class Land_HBarrierWall6_F {
+    class ACE_EntrenchingTool {
+        kindOf = "Items";
         displayName = "Heavy Package";
-        description = "1x H-Barrier Wall (long)<br/>2x H-Barrier Wall (short)<br/>2x Concrete Barrier (medium)<br/>2x Concrete Barrier";
+        description = "1x Entrenching Tool<br/>1x H-Barrier Wall (long)<br/>2x H-Barrier Wall (short)<br/>2x Concrete Barrier (medium)<br/>2x Concrete Barrier";
 
-        price = 2300;
+        price = 2400;
         stock = 5;
 
         code = "if (player == _this select 0) then {[player,'Land_HBarrierWall6_F',1] call grad_fortifications_fnc_addFort; [player,'Land_HBarrierWall4_F',2] call grad_fortifications_fnc_addFort; [player,'Land_CncBarrierMedium_F',2] call grad_fortifications_fnc_addFort; [player,'Land_CncBarrier_F',2] call grad_fortifications_fnc_addFort};";
