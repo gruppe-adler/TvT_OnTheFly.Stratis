b2a7d95b9b39fadc0487a9870eea777c951b68c8
McDiod <hobbeldobbel@gmail.com>
2020-05-10 15:35:38 +0200

    edit isr_idf

--- buyables/vehicles/isr_idf.hpp
+++ buyables/vehicles/isr_idf.hpp
@@ -3,10 +3,9 @@ class Vehicles: vehicles_base {
     class B_Quadbike_01_F: B_Quadbike_01_F {};
     class Box_NATO_Equip_F: Box_NATO_Equip_F {};
 
-
     class rhsusf_m998_w_2dr_halftop {
         condition = "OTF_ISWOODLAND";
-        price = 1800;
+        price = 4000;
         stock = 10;
     };
 
@@ -16,21 +15,11 @@ class Vehicles: vehicles_base {
 
     class rhsusf_m1025_w_m2 {
         condition = "OTF_ISWOODLAND";
-        price = 4000;
+        price = 10000;
         stock = 10;
     };
 
     class rhsusf_m1025_d_m2: rhsusf_m1025_w_m2 {
-        condition = "!OTF_ISWOODLAND";
-    };
-
-    class rhsusf_m113_usarmy {
-        condition = "OTF_ISWOODLAND";
-        price = 6000;
-        stock = 10;
-    };
-
-    class rhsusf_m113d_usarmy: rhsusf_m113_usarmy {
         condition = "!OTF_ISWOODLAND";
     };
 };
