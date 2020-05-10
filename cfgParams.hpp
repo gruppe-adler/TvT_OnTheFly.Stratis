cc1ec02bc4a4ba37f45d4c9e729b25858e4dee6d
McDiod <hobbeldobbel@gmail.com>
2020-05-10 16:09:18 +0200

    edit us_army

--- cfgParams.hpp
+++ cfgParams.hpp
@@ -21,7 +21,7 @@ class Params
         title = "Blufor Faction";
         values[] = {0,1,2,3,4,5,6,7,8,9,10,11,12};
         texts[] = {
-            "USA - Army",
+            "USA - Army - [RHS:USAF]",
             "USA - Marines - [RHS:USAF]",
             "Russia - MSV",
             "Russia - Spetsnaz - [RHS:AFRF]",
@@ -43,7 +43,7 @@ class Params
         title = "Opfor Faction";
         values[] = {0,1,2,3,4,5,6,7,8,9,10,11,12};
         texts[] = {
-            "USA - Army",
+            "USA - Army - [RHS:USAF]",
             "USA - Marines - [RHS:USAF]",
             "Russia - MSV",
             "Russia - Spetsnaz - [RHS:AFRF]",
