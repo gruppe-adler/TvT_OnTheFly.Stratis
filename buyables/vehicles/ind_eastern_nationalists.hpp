4e362af96efa2c7fabff481d33d465ad17c39bf7
McDiod <hobbeldobbel@gmail.com>
2020-05-10 16:25:08 +0200

    edit ind_eastern_nationalists

--- buyables/vehicles/ind_eastern_nationalists.hpp
+++ buyables/vehicles/ind_eastern_nationalists.hpp
@@ -3,18 +3,13 @@ class Vehicles: vehicles_base {
     class B_Quadbike_01_F: B_Quadbike_01_F {};
     class Box_NATO_Equip_F: Box_NATO_Equip_F {};
 
-    class rhsgref_ins_g_ural {
-        price = 1500;
+    class rhsgref_ins_g_uaz {
+        price = 3500;
         stock = 10;
     };
 
     class rhsgref_ins_g_uaz_dshkm_chdkz {
-        price = 2500;
-        stock = 10;
-    };
-
-    class rhsgref_BRDM2_ins_g {
-        price = 4000;
+        price = 7500;
         stock = 10;
     };
 };
