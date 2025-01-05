bb4c40057ec89fab83bcc6daf7bc663c83093e75
nomisum <nomisum@gmail.com>
2025-01-05 12:03:18 +0100

    add more vehicles to late factions

--- buyables/vehicles/spe_wehrmacht_late.hpp
+++ buyables/vehicles/spe_wehrmacht_late.hpp
@@ -2,21 +2,39 @@ class Vehicles: vehicles_base {
 
     class Box_NATO_Equip_F: Box_NATO_Equip_F {};
 
-	class SPE_OpelBlitz {
+    class SPE_OpelBlitz {
+        condition = "true";
+        price = 250;
+        stock = 2;
+    };
+    
+    class SPE_ST_R200_Hood {
+        condition = "true";
+        price = 500;
+        stock = 2;
+    }
+
+    class SPE_GER_R200_MG34 {
         condition = "true";
         price = 1000;
-        stock = 10;
-    };
+        stock = 2;
+    }
 
     class SPE_SdKfz250_1 {
         condition = "true";
         price = 4500;
-        stock = 10;
+        stock = 2;
     };
 
     class SPE_PzKpfwVI_H1 {
         condition = "true";
         price = 5500;
-        stock = 10;
+        stock = 2;
     };
+
+    class SPE_ST_Jagdpanther_G1 {
+        condition = "true";
+        price = 7500;
+        stock = 1;
+    }
 };
